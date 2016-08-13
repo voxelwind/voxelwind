3c03b1abc628e1e2f774b836ad4ef7f091b25ea7
Tux <write@imaginarycode.com>
2016-08-13 10:21:46 -0400

    Document annotations.

--- src/main/java/com/voxelwind/server/network/mcpe/annotations/DisallowWrapping.java
+++ src/main/java/com/voxelwind/server/network/mcpe/annotations/DisallowWrapping.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to ignore wrapping this packet in a {@link com.voxelwind.server.network.mcpe.packets.McpeWrapper}. This is used
+ * primarily for RakNet packets sent during a player session, such as pong packets.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface DisallowWrapping {
--- src/main/java/com/voxelwind/server/network/mcpe/annotations/ForceClearText.java
+++ src/main/java/com/voxelwind/server/network/mcpe/annotations/ForceClearText.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation forces any packet sent to be sent in clear text. This is primarily used for handshaking and RakNet
+ * packets.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface ForceClearText {
