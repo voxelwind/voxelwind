dfcff248ca6744f0561d0dfd2e6219d49639e0d9
Tux <write@imaginarycode.com>
2016-08-17 18:40:27 -0400

    Wool color enum

---
+++ api/src/main/java/com/voxelwind/api/util/WoolColor.java
@@ -0,0 +1,23 @@
+package com.voxelwind.api.util;
+
+/**
+ * Represents all wool colors.
+ */
+public enum WoolColor {
+    WHITE,
+    ORANGE,
+    MAGENTA,
+    LIGHT_BLUE,
+    YELLOW,
+    LIME,
+    PINK,
+    GRAY,
+    LIGHT_GRAY,
+    CYAN,
+    PURPLE,
+    BLUE,
+    BROWN,
+    GREEN,
+    RED,
+    BLACK
+}
