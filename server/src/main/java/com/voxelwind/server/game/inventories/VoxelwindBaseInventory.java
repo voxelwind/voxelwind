f548007d41bf287ea6a70c9fc1d635ad33f13df2
Tux <write@imaginarycode.com>
2016-08-26 16:42:02 -0400

    Fixed setAllContents()

--- server/src/main/java/com/voxelwind/server/game/inventories/VoxelwindBaseInventory.java
+++ server/src/main/java/com/voxelwind/server/game/inventories/VoxelwindBaseInventory.java
@@ -6,6 +6,7 @@ import com.voxelwind.api.game.inventories.Inventory;
 import com.voxelwind.api.game.item.ItemStack;
 
 import javax.annotation.Nonnull;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -72,6 +73,15 @@ public abstract class VoxelwindBaseInventory implements Inventory {
     @Override
     public void setAllContents(@Nonnull Map<Integer, ItemStack> contents) {
         Preconditions.checkNotNull(contents, "contents");
+        Map<Integer, ItemStack> contentsCopy = ImmutableMap.copyOf(contents);
+        if (contentsCopy.isEmpty()) {
+            inventory.clear();
+            return;
+        }
+
+        Integer maxSlot = Collections.max(contentsCopy.keySet());
+        Preconditions.checkArgument(maxSlot < fullSize, "Maximum passed contents slot (%s) is greater than this inventory's size (%s)",
+                maxSlot, fullSize);
         inventory.clear();
         inventory.putAll(ImmutableMap.copyOf(contents));
     }
