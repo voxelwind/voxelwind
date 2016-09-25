bf65b5b9f8290537954c21ab9160a11d266209b5
Tux <write@imaginarycode.com>
2016-09-25 16:40:46 -0400

    Fix test failures on Windows
    Also adds commons-lang for some useful utilities for plugins to use

--- server/src/test/java/com/voxelwind/server/game/level/provider/anvil/util/AnvilRegionReaderTest.java
+++ server/src/test/java/com/voxelwind/server/game/level/provider/anvil/util/AnvilRegionReaderTest.java
@@ -1,9 +1,9 @@
 package com.voxelwind.server.game.level.provider.anvil.util;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.Test;
 
 import java.io.InputStream;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
@@ -12,6 +12,9 @@ public class AnvilRegionReaderTest {
     @Test
     public void hasChunk() throws Exception {
         String path = getClass().getClassLoader().getResource("example_region.mca").getFile();
+        if (SystemUtils.IS_OS_WINDOWS) {
+            path = path.substring(1);
+        }
         try (AnvilRegionReader reader = new AnvilRegionReader(Paths.get(path))) {
             assertTrue("chunk (14, 2) is missing", reader.hasChunk(14, 2));
         }
@@ -20,6 +23,9 @@ public class AnvilRegionReaderTest {
     @Test
     public void readChunk() throws Exception {
         String path = getClass().getClassLoader().getResource("example_region.mca").getFile();
+        if (SystemUtils.IS_OS_WINDOWS) {
+            path = path.substring(1);
+        }
         try (AnvilRegionReader reader = new AnvilRegionReader(Paths.get(path))) {
             assertTrue("chunk (14, 2) is missing", reader.hasChunk(14, 2));
             try (InputStream stream = reader.readChunk(14, 2)) {
