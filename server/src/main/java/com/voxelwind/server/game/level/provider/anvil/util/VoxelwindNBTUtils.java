014236b93ea5dd5717f4564d551fe567a125f043
Tux <write@imaginarycode.com>
2016-09-28 20:01:23 -0400

    Add NBT block entity inventory deserialization

--- server/src/main/java/com/voxelwind/server/game/level/provider/anvil/util/VoxelwindNBTUtils.java
+++ server/src/main/java/com/voxelwind/server/game/level/provider/anvil/util/VoxelwindNBTUtils.java
@@ -1,8 +1,7 @@
 package com.voxelwind.server.game.level.provider.anvil.util;
 
-import com.flowpowered.nbt.ByteTag;
-import com.flowpowered.nbt.CompoundMap;
-import com.flowpowered.nbt.ShortTag;
+import com.flowpowered.nbt.*;
+import com.google.common.base.Preconditions;
 import com.voxelwind.api.game.item.ItemStack;
 import com.voxelwind.api.game.item.ItemType;
 import com.voxelwind.api.game.item.ItemTypes;
@@ -17,5 +16,21 @@ public class VoxelwindNBTUtils {
         // TODO: "tag" compound
         ItemType type = ItemTypes.forId(idTag.getValue());
         return new VoxelwindItemStack(type, countTag.getValue(), type.createDataFor(damageTag.getValue()).orElse(null));
+    }
+
+    public static ItemStack[] createItemStacks(ListTag<CompoundTag> tag, int knownSize) {
+        ItemStack[] all = new ItemStack[knownSize];
+        for (CompoundTag slotTag : tag.getValue()) {
+            CompoundMap slotMap = slotTag.getValue();
+            Tag<?> inSlotTagRaw = slotMap.get("Slot");
+            Preconditions.checkArgument(inSlotTagRaw != null, "Slot NBT tag is missing from compound");
+            Preconditions.checkArgument(inSlotTagRaw instanceof ByteTag, "Slot NBT tag is not a Byte");
+            ByteTag inSlotTag = (ByteTag) inSlotTagRaw;
+            if (inSlotTag.getValue() < 0 || inSlotTag.getValue() >= knownSize) {
+                throw new IllegalArgumentException("Found illegal slot " + inSlotTag.getValue());
+            }
+            all[inSlotTag.getValue()] = createItemStack(slotMap);
+        }
+        return all;
     }
 }
