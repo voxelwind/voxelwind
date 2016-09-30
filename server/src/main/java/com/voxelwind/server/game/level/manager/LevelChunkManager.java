c2fe0e90c6557f08af30ab1a09fb0ed94160681f
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2016-09-30 09:52:54 +0200

    Use Trove (synchronized) instead of ConcurrentHashMaps.
    They are faster for the kind of work we do. Also they have a smaller memory footprint. The way the Chunks are loaded (async) is not affected.

--- server/src/main/java/com/voxelwind/server/game/level/manager/LevelChunkManager.java
+++ server/src/main/java/com/voxelwind/server/game/level/manager/LevelChunkManager.java
@@ -1,14 +1,16 @@
 package com.voxelwind.server.game.level.manager;
 
-import com.flowpowered.math.vector.Vector2i;
 import com.voxelwind.api.game.level.Chunk;
 import com.voxelwind.api.game.level.Level;
 import com.voxelwind.server.game.level.provider.ChunkProvider;
+import gnu.trove.TCollections;
+import gnu.trove.map.TLongObjectMap;
+import gnu.trove.map.hash.TLongObjectHashMap;
 
 import javax.annotation.Nonnull;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -19,8 +21,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * loaded asynchronously and only one attempt to load the same chunk is made, even with concurrent callers.
  */
 public class LevelChunkManager {
-    private final Map<Vector2i, Chunk> chunksLoaded = new ConcurrentHashMap<>();
-    private final Map<Vector2i, LoadingTask> chunksToLoad = new ConcurrentHashMap<>();
+    private final TLongObjectMap<Chunk> chunksLoaded = TCollections.synchronizedMap(new TLongObjectHashMap<Chunk>());
+    private final TLongObjectMap<LoadingTask> chunksToLoad = TCollections.synchronizedMap(new TLongObjectHashMap<LoadingTask>());
     private final Level level;
     private final ChunkProvider backingChunkProvider;
 
@@ -36,18 +38,23 @@ public class LevelChunkManager {
         }
 
         // Not already loaded, so we need to ask the chunk provider.
-        Vector2i coords = new Vector2i(x, z);
-        LoadingTask loadingTask = chunksToLoad.computeIfAbsent(coords, LoadingTask::new);
+        long chunkKey = toLong(x, z);
+        LoadingTask loadingTask = chunksToLoad.get(chunkKey);
+        if (loadingTask == null) {
+            loadingTask = new LoadingTask(x, z);
+            chunksToLoad.put(chunkKey, loadingTask);
+        }
+
         CompletableFuture<Chunk> resultFuture = loadingTask.createCompletableFuture();
         if (!loadingTask.isInitiated()) {
             loadingTask.execute();
         }
+
         return resultFuture;
     }
 
     public Optional<Chunk> getChunkIfLoaded(int x, int z) {
-        Vector2i coords = new Vector2i(x, z);
-        return Optional.ofNullable(chunksLoaded.get(coords));
+        return Optional.ofNullable(chunksLoaded.get(toLong(x, z)));
     }
 
     public void onTick() {
@@ -61,9 +68,9 @@ public class LevelChunkManager {
         private final AtomicBoolean initiated = new AtomicBoolean(false);
         private final AtomicBoolean done = new AtomicBoolean(false);
 
-        private LoadingTask(Vector2i vector2i) {
-            this.x = vector2i.getX();
-            this.z = vector2i.getY();
+        private LoadingTask(int x, int z) {
+            this.x = x;
+            this.z = z;
         }
 
         boolean isInitiated() {
@@ -76,11 +83,13 @@ public class LevelChunkManager {
 
             // Load the chunk.
             backingChunkProvider.createChunk(level, x, z).whenComplete((chunk, throwable) -> {
+                long chunkKey = toLong(x, z);
                 if (chunk != null) {
-                    chunksLoaded.put(new Vector2i(x, z), chunk);
+                    chunksLoaded.put(chunkKey, chunk);
                 }
+
                 done.set(true);
-                chunksToLoad.remove(new Vector2i(x, z));
+                chunksToLoad.remove(chunkKey);
                 for (CompletableFuture<Chunk> future : futuresToComplete) {
                     if (throwable != null) {
                         future.completeExceptionally(throwable);
@@ -102,9 +111,21 @@ public class LevelChunkManager {
                 exceptionalCompleted.completeExceptionally(new IllegalStateException("Failed to load chunk."));
                 return exceptionalCompleted;
             }
+
             CompletableFuture<Chunk> completableFuture = new CompletableFuture<>();
             futuresToComplete.add(completableFuture);
             return completableFuture;
         }
+    }
+
+    /**
+     * Shift two int's together to form a compound key
+     *
+     * @param x value of key
+     * @param z value of key
+     * @return long compound of the two int's
+     */
+    private static long toLong(int x, int z) {
+        return ((long) x << 32) + z - Integer.MIN_VALUE;
     }
 }
