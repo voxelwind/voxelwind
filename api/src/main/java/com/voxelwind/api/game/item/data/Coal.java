a11529aca306af021308c4e9a39792b1b4aaba9c
Tux <write@imaginarycode.com>
2016-10-05 12:39:47 -0400

    Convert Coal class into an enum

--- api/src/main/java/com/voxelwind/api/game/item/data/Coal.java
+++ api/src/main/java/com/voxelwind/api/game/item/data/Coal.java
@@ -8,47 +8,26 @@ import java.util.Objects;
  * This {@link Metadata} represents coal. In Minecraft, coal can either be regular or be charcoal. They are the same,
  * regardless.
  */
-public final class Coal implements Metadata {
+public enum Coal implements Metadata {
     /**
      * Represents a regular coal item.
      */
-    public static final Coal REGULAR = new Coal(false);
+    REGULAR,
     /**
      * Represents a charcoal item.
      */
-    public static final Coal CHARCOAL = new Coal(true);
-
-    private final boolean isCharcoal;
-
-    private Coal(boolean isCharcoal) {
-        this.isCharcoal = isCharcoal;
-    }
+    CHARCOAL;
 
     /**
      * Returns whether or not this item is charcoal.
      * @return whether or not this item is charcoal
      */
     public final boolean isCharcoal() {
-        return isCharcoal;
-    }
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Coal coal = (Coal) o;
-        return isCharcoal == coal.isCharcoal;
-    }
-
-    @Override
-    public final int hashCode() {
-        return Objects.hash(isCharcoal);
+        return this == CHARCOAL;
     }
 
     @Override
     public final String toString() {
-        return "Coal{" +
-                "isCharcoal=" + isCharcoal +
-                '}';
+        return "Coal{" + name() + '}';
     }
 }
