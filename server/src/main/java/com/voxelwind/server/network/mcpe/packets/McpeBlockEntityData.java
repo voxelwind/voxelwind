9f52259646a148787af98d4160a148a04b5fc5f1
Tux <write@imaginarycode.com>
2016-10-26 16:07:32 -0400

    Fix missing network NBT stuff

--- server/src/main/java/com/voxelwind/server/network/mcpe/packets/McpeBlockEntityData.java
+++ server/src/main/java/com/voxelwind/server/network/mcpe/packets/McpeBlockEntityData.java
@@ -1,6 +1,7 @@
 package com.voxelwind.server.network.mcpe.packets;
 
 import com.flowpowered.math.vector.Vector3i;
+import com.voxelwind.nbt.io.NBTEncoding;
 import com.voxelwind.nbt.io.NBTWriter;
 import com.voxelwind.nbt.tags.Tag;
 import com.voxelwind.server.network.NetworkPackage;
@@ -25,7 +26,7 @@ public class McpeBlockEntityData implements NetworkPackage {
     @Override
     public void encode(ByteBuf buffer) {
         McpeUtil.writeBlockCoords(buffer, position);
-        try (NBTWriter writer = new NBTWriter(new ByteBufOutputStream(buffer.order(ByteOrder.LITTLE_ENDIAN)))) {
+        try (NBTWriter writer = new NBTWriter(new ByteBufOutputStream(buffer.order(ByteOrder.LITTLE_ENDIAN)), NBTEncoding.MCPE_0_16_NETWORK)) {
             writer.write(blockEntityData);
         } catch (IOException e) {
             throw new RuntimeException(e);
