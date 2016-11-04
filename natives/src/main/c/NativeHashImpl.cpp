1c766557c13e2e7802b20e3d6e8cff3a4566a4c3
Tux <write@imaginarycode.com>
2016-11-04 18:07:23 -0400

    Simplified NativeHashImpl
    My knowledge of C++ is seriously lacking.

--- natives/src/main/c/NativeHashImpl.cpp
+++ natives/src/main/c/NativeHashImpl.cpp
@@ -15,29 +15,11 @@ void JNICALL Java_com_voxelwind_server_jni_hash_NativeHashImpl_update(JNIEnv *en
     EVP_DigestUpdate((EVP_MD_CTX*) ctx, (byte*) in, bytes);
 }
 
-// This is from the existing native functions and may not even be needed. TODO
-jint throwException(JNIEnv *env, const char* message, int err) {
-    // These can't be static for some unknown reason
-    jclass exceptionClass = env->FindClass("net/md_5/bungee/jni/NativeCodeException");
-    jmethodID exceptionInitID = env->GetMethodID(exceptionClass, "<init>", "(Ljava/lang/String;I)V");
-
-    jstring jMessage = env->NewStringUTF(message);
-
-    jthrowable throwable = (jthrowable) env->NewObject(exceptionClass, exceptionInitID, jMessage, err);
-    return env->Throw(throwable);
-}
-
 jbyteArray JNICALL Java_com_voxelwind_server_jni_hash_NativeHashImpl_digest(JNIEnv *env, jobject obj, jlong ctx) {
     // SHA-256 produces 32 bytes (256 bits)
-    void *output = malloc(32);
-    if (output == NULL) {
-        throwException(env, "Ran out of memory while allocating memory for SHA256 output: ", 0);
-    }
+    unsigned char output[32];
     EVP_DigestFinal((EVP_MD_CTX*) ctx, (unsigned char*) output, NULL);
     jbyteArray array = env->NewByteArray(32);
     env->SetByteArrayRegion(array, 0, 32, (jbyte*) output);
-
-    // Output is no longer required.
-    free(output);
     return array;
 }
