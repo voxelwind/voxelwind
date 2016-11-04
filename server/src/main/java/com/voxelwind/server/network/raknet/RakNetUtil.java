a24f275b8d1f44dbbcd1e41c8c39658306825a0a
Tux <write@imaginarycode.com>
2016-11-04 17:50:14 -0400

    Added IPv6 support.

--- server/src/main/java/com/voxelwind/server/network/raknet/RakNetUtil.java
+++ server/src/main/java/com/voxelwind/server/network/raknet/RakNetUtil.java
@@ -41,39 +41,41 @@ public class RakNetUtil {
         }
     }
 
-    public static InetAddress readAddress(ByteBuf buf) {
+    public static InetSocketAddress readSocketAddress(ByteBuf buf) {
         short type = buf.readUnsignedByte();
         if (type == 4) {
             byte[] addr = new byte[4];
             buf.readBytes(addr);
             try {
-                return InetAddress.getByAddress(addr);
+                return new InetSocketAddress(InetAddress.getByAddress(addr), buf.readUnsignedShort());
+            } catch (UnknownHostException e) {
+                // ;_;
+                throw new IllegalArgumentException(e);
+            }
+        } else if (type == 6) {
+            buf.skipBytes(2); // Family (always AF_INET6)
+            int port = buf.readUnsignedShort();
+            buf.skipBytes(8); // Flow info, not at all needed.
+            byte[] addr = new byte[16];
+            buf.readBytes(addr);
+            try {
+                return new InetSocketAddress(InetAddress.getByAddress(addr), port);
             } catch (UnknownHostException e) {
                 // ;_;
                 throw new IllegalArgumentException(e);
             }
         } else {
-            throw new UnsupportedOperationException("Can't deserialize an IPv6 address.");
-        }
-    }
-
-    public static InetSocketAddress readSocketAddress(ByteBuf buf) {
-        InetAddress address = readAddress(buf);
-        int port = buf.readUnsignedShort();
-        return new InetSocketAddress(address, port);
-    }
-
-    public static void writeAddress(ByteBuf buf, InetAddress address) {
-        if (address instanceof Inet4Address) {
-            buf.writeByte((4 & 0xFF));
-            buf.writeBytes(address.getAddress());
-        } else {
-            throw new UnsupportedOperationException("Can't serialize an IPv6 address.");
+            throw new UnsupportedOperationException("Can't deserialize an unrecognized address.");
         }
     }
 
     public static void writeSocketAddress(ByteBuf buf, InetSocketAddress address) {
-        writeAddress(buf, address.getAddress());
-        buf.writeShort(address.getPort());
+        if (address.getAddress() instanceof Inet4Address) {
+            buf.writeByte((4 & 0xFF));
+            buf.writeBytes(address.getAddress().getAddress());
+            buf.writeShort(address.getPort());
+        } else {
+            throw new UnsupportedOperationException("Can't serialize an IPv6 address.");
+        }
     }
 }
